f828bb414b3049ad27f882d0067330b158aebd36
Ritvik19 <rastogiritvik99@gmail.com>
2020-02-04 22:32:45 +0530

    Password

--- clipboard.py
+++ clipboard.py
@@ -12,7 +12,6 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = '79e9d3b5d183b6e620e3776f77d95f4b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 admin = Admin(app)
-# admin.add_view(ModelView(Clip, db.session))
 
 class Clip(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
@@ -21,7 +20,7 @@ class Clip(db.Model):
     private = db.Column(db.Boolean, nullable=False, default=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     duration = db.Column(db.DateTime, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False, default='')
     key = db.Column(db.String(32), nullable=False)
 
 admin.add_view(ModelView(Clip, db.session))
@@ -34,11 +33,16 @@ def home():
         print('query')
         print(request.form)
         existing_clip = Clip.query.filter_by(key=request.form.get("key")).first()
-        data = {
-            'title': existing_clip.title, 'clip': existing_clip.clip, 'duration': existing_clip.duration.strftime("%b %d, %Y"),
-            'password': existing_clip.password
-        }
-        return render_template('clip.html', key=request.form.get("key"), data=data)
+        if encrypt_md5(request.form.get('password')) == existing_clip.password:    
+            data = {
+                'title': existing_clip.title, 'clip': existing_clip.clip, 'duration': existing_clip.duration.strftime("%b %d, %Y"),
+                'password': ''
+            }
+            return render_template('clip.html', key=request.form.get("key"), data=data)
+        else:
+            print(encrypt_md5(request.form.get('password')))
+            print(existing_clip.password)
+            print('Password Error')
     pnum = request.args.get('page', 1, int)
     return render_template('home.html', clips=Clip.query.filter_by(private=False).order_by(Clip.uid.desc()).paginate(pnum, 5))
 
@@ -51,7 +55,8 @@ def clip():
             if len(Clip.query.filter_by(key=request.form.get("key")).all()) == 0:
                 new_clip = Clip(title=request.form.get("title"), clip=request.form.get("clip"), 
                             duration=datetime.strptime(request.form.get("duration"), "%b %d, %Y")
-                            , password=request.form.get("password"), private=bool(request.form.get("private", False)),
+                            , password=encrypt_md5(request.form.get("password")), 
+                            private=bool(request.form.get("private", False)),
                             key=request.form.get("key"))
                 db.session.add(new_clip)
             else:
@@ -59,7 +64,7 @@ def clip():
                 existing_clip.itle=request.form.get("title")
                 existing_clip.clip=request.form.get("clip")
                 existing_clip.duration=datetime.strptime(request.form.get("duration"), "%b %d, %Y")
-                existing_clip.password=request.form.get("password")
+                existing_clip.password = encrypt_md5(request.form.get("password"))
                 existing_clip.private=bool(request.form.get("private", False))
             db.session.commit()
         if request.form.get('submit') == 'Delete':
